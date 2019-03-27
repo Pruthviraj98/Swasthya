896d73babadbe5df28552354e1353a89e2203361
Pruthviraj Patil <pruthvirajr.patil2016@vitstudent.ac.in>
2019-03-28 00:26:51 +0530

    update
    280319

--- serve.js
+++ serve.js
@@ -147,6 +147,7 @@ app.post('/route_create', function(request, response){
 });
 app.post('/logout', function(request, response){
   request.session.loggedin=false;
+  request.session.destroy();
   console.log("I am loggedout");
   response.redirect('/');
 });
@@ -185,8 +186,98 @@ app.post('/getSearchResults', function(request, response){
   console.log(request.body.quer)
   connection.query("SELECT * FROM diseases WHERE symptoms LIKE '%"+queryy+"%'", function(err, results, fields){
               if (!err) {
-                    console.log(results);
-                  	response.json(results);
+                request.session.S0name=results[0].name;
+                request.session.S0diseases=results[0].disease;
+                request.session.S0symptoms=results[0].symptoms;
+                request.session.S0date_of_log=results[0].date_of_log;
+                request.session.S0date_of_cure=results[0].date_of_cure;
+                request.session.S0hospitals=results[0].hospital_visited;
+                request.session.S0steps=results[0].steps_taken;
+                request.session.S0phno=results[0].phno;
+
+                request.session.S1name=results[1].name;
+                request.session.S1diseases=results[1].disease;
+                request.session.S1symptoms=results[1].symptoms;
+                request.session.S1date_of_log=results[1].date_of_log;
+                request.session.S1date_of_cure=results[1].date_of_cure;
+                request.session.S1hospitals=results[1].hospital_visited;
+                request.session.S1steps=results[1].steps_taken;
+                request.session.S1phno=results[1].phno;
+
+                request.session.S2name=results[2].name;
+                request.session.S2diseases=results[2].disease;
+                request.session.S2symptoms=results[2].symptoms;
+                request.session.S2date_of_log=results[2].date_of_log;
+                request.session.S2date_of_cure=results[2].date_of_cure;
+                request.session.S2hospitals=results[2].hospital_visited;
+                request.session.S2steps=results[2].steps_taken;
+                request.session.S2phno=results[2].phno;
+
+                request.session.S3name=results[3].name;
+                request.session.S3diseases=results[3].disease;
+                request.session.S3symptoms=results[3].symptoms;
+                request.session.S3date_of_log=results[3].date_of_log;
+                request.session.S3date_of_cure=results[3].date_of_cure;
+                request.session.S3hospitals=results[3].hospital_visited;
+                request.session.S3steps=results[3].steps_taken;
+                request.session.S3phno=results[3].phno;
+
+                request.session.S4name=results[4].name;
+                request.session.S4diseases=results[4].disease;
+                request.session.S4symptoms=results[4].symptoms;
+                request.session.S4date_of_log=results[4].date_of_log;
+                request.session.S4date_of_cure=results[4].date_of_cure;
+                request.session.S4hospitals=results[4].hospital_visited;
+                request.session.S4steps=results[4].steps_taken;
+                request.session.S4phno=results[4].phno;
+
+                    response.render('searchResults.html', {
+                      name1:request.session.S0name,
+                      symptoms1:request.session.S0symptoms,
+                      date_log1:request.session.S0date_of_log,
+                      date_cure1:request.session.S0date_of_cure,
+                      hospitals1:request.session.S0hospitals,
+                      steps1:request.session.S0steps,
+                      phno1:request.session.S0phno,
+                      diseases1:request.session.S0diseases,
+
+                      name2:request.session.S1name,
+                      symptoms2:request.session.S1symptoms,
+                      date_log2:request.session.S1date_of_log,
+                      date_cure2:request.session.S1date_of_cure,
+                      hospitals2:request.session.S1hospitals,
+                      steps2:request.session.S1steps,
+                      phno2:request.session.S1phno,
+                      diseases2:request.session.S1diseases,
+
+                      name3:request.session.S2name,
+                      symptoms3:request.session.S2symptoms,
+                      date_log3:request.session.S2date_of_log,
+                      date_cure3:request.session.S2date_of_cure,
+                      hospitals3:request.session.S2hospitals,
+                      steps3:request.session.S2steps,
+                      phno3:request.session.S2phno,
+                      diseases3:request.session.S2diseases,
+
+                      name4:request.session.S3name,
+                      symptoms4:request.session.S3symptoms,
+                      date_log4:request.session.S3date_of_log,
+                      date_cure4:request.session.S3date_of_cure,
+                      hospitals4:request.session.S3hospitals,
+                      steps4:request.session.S3steps,
+                      phno4:request.session.S3phno,
+                      diseases4:request.session.S3diseases,
+
+                      name5:request.session.S4name,
+                      symptoms5:request.session.S4symptoms,
+                      date_log5:request.session.S4date_of_log,
+                      date_cure5:request.session.S4date_of_cure,
+                      hospitals5:request.session.S4hospitals,
+                      steps5:request.session.S4steps,
+                      phno5:request.session.S4phno,
+                      diseases5:request.session.S4diseases,
+                    });
+//                  	response.json(results);
           		} else {
           			console.log('Results not found.');
           		}
